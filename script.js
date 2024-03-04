9cb45553f030e769114a04ef48ef58904ebb0101
Higor Lara <higorv.lara@gmail.com>
2024-03-04 15:30:50 -0300

    Script JS created

---
+++ script.js
@@ -0,0 +1,8 @@
+const html = document.querySelector('html')
+const mainButtons = document.querySelectorAll('.app__card-button')
+
+for (let counter = 0; index < mainButtons.length; counter++) {
+    mainButtons[counter].addEventListener('click', () => {
+        
+    })
+}
