eadfd999e051062899c69cfe1d149b5f105f4580
Higor Lara <higorv.lara@gmail.com>
2024-03-06 12:12:51 -0300

    Changing the template.
    Getting all buttons that change the template of the page and making it work.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const html = document.querySelector('html')
 const mainButtons = document.querySelectorAll('.app__card-button')
 
-for (let counter = 0; index < mainButtons.length; counter++) {
+for (let counter = 0; counter < mainButtons.length; counter++) {
     mainButtons[counter].addEventListener('click', () => {
-        
-    })
+        const template = mainButtons[counter].classList[2]
+        console.log(template)
+        html.setAttribute  ('data-contexto', template)
+    }) 
 }
