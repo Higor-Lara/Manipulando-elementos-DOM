e390eb213802f11254e9fb15063a206f01d2fb76
Higor Lara <higorv.lara@gmail.com>
2024-03-11 17:11:18 -0300

    Setting the banner

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const html = document.querySelector('html')
 const mainButtons = document.querySelectorAll('.app__card-button')
+const banner = document.querySelector('.app__image')
 
 for (let counter = 0; counter < mainButtons.length; counter++) {
     mainButtons[counter].addEventListener('click', () => {
         const template = mainButtons[counter].classList[2]
-        console.log(template)
         html.setAttribute  ('data-contexto', template)
-    }) 
+        banner.setAttribute('src', `/imagens/${template}.png`)
+    })
 }
