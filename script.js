0e2b3aeac69ac85703d01d95ee46e54196e72431
Higor Lara <higorv.lara@gmail.com>
2024-03-12 17:09:27 -0300

    Changing the title on clinck
    Using switch and innerHTML to change the title when the user click on the button.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const html = document.querySelector('html')
 const mainButtons = document.querySelectorAll('.app__card-button')
+const banner = document.querySelector('.app__image')
 const timerElement = document.querySelector('#timer')
 const titleBanner = document.querySelector('.app__title')
 const startPause = document.querySelector('.app__card-primary-button')
@@ -13,5 +14,19 @@ for (let counter = 0; counter < mainButtons.length; counter++) {
         const template = mainButtons[counter].classList[2]
         html.setAttribute  ('data-contexto', template)
         banner.setAttribute('src', `/imagens/${template}.png`)
+
+        switch (template) {
+            case "foco":
+                titleBanner.innerHTML = `Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>`
+                break;
+            case "descanso-curto":
+                titleBanner.innerHTML = `Que tal dar uma respirada?<br>
+                <strong class="app__title-strong">Faça uma pausa curta!</strong>`
+                break;
+            case "descanso-longo":
+                titleBanner.innerHTML = `Hora de voltar a superfície<br>
+                <strong class="app__title-strong">Faça uma pausa longa.</strong>`
+        }
     })
 }
