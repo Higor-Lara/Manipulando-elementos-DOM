b921dab16d9019fb7a52efbe907c2881125e1bf3
Higor Lara <higorv.lara@gmail.com>
2024-03-12 16:36:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const html = document.querySelector('html')
 const mainButtons = document.querySelectorAll('.app__card-button')
+const timerElement = document.querySelector('#timer')
+const banner = document.querySelector('.app__image')
+const titleBanner = document.querySelector('.app__title')
+const startPause = document.querySelector('.app__card-primary-button')
+
+const focusTime = 1500
+const shortRest = 300
+const longRest = 900
 
 for (let counter = 0; counter < mainButtons.length; counter++) {
     mainButtons[counter].addEventListener('click', () => {
